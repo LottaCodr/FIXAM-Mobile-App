6051042bb4bbf836b4ecac9bf3dfcf63431e1bd4
King-Lota <lottanna47@gmail.com>
2026-01-20 09:21:47 +0100

    Remove unused FilterPill components (DistancePill, PricePill, RatingPill) to streamline category filtering functionality.

---
+++ app/(screens)/category/component/FilterPills/FilterBottomSheet.tsx
@@ -0,0 +1,31 @@
+import { useArtisanStore } from "@/store/artisan.store";
+import { useTheme } from "@/theme/ThemeProvider";
+import { Pressable, Text, View } from "react-native";
+
+
+
+export default function FilterBottomSheet({ onClose }: { onClose: () => void }) {
+
+    const { spacing, colors, radius, typography } = useTheme();
+    const setFilters = useArtisanStore((s) => s.setFilters)
+
+    return (
+        <View
+            style={{
+                backgroundColor: colors.surface,
+                padding: spacing[5],
+                borderTopLeftRadius: radius.xl,
+                borderTopRightRadius: radius.xl
+            }}
+        >
+            <Text style={{ fontSize: typography.body }}>Filters</Text>
+
+            <Pressable onPress={() => {
+                setFilters({ maxDistance: 5 });
+                onClose();
+            }}>
+                <Text>Within 5km</Text>
+            </Pressable>
+        </View>
+    )
+}
